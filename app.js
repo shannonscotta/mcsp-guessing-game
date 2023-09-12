5de80246894653c20fcf7d505618e5099910130d
Scott Shannon <scott.a.shannon@outlook.com>
2023-09-11 19:37:51 -0700

    finish guessing game

--- app.js
+++ app.js
@@ -1,35 +1,43 @@
-let SECRET_NUMBER = randomNumber(1, 5);
+let userData = {};
 
-let playAgain = false;
-let userName = prompt("What is your name?");
-let userGuess = prompt(`Hello ${userName}, guess a number.`);
-let validNumber;
-let guessArr = [];
+function playGame() {
+  let SECRET_NUMBER = randomNumber(1, 5);
+  let playAgain = false;
+  let userName = prompt("What is your name?");
+  let userGuess = prompt(`Hello ${userName}, guess a number.`);
+  let validNumber;
+  let guessArr = [];
 
-while (validNumber !== SECRET_NUMBER) {
-  guessArr.push(userGuess);
+  while (validNumber !== SECRET_NUMBER) {
+    guessArr.push(userGuess);
 
-  Number(userGuess) > 0 || Number(userGuess) < 0
-    ? (validNumber = Number(userGuess))
-    : (userGuess = prompt(`${userGuess} is not a number, enter a NUMBER.`));
+    Number(userGuess) > 0 || Number(userGuess) < 0
+      ? (validNumber = Number(userGuess))
+      : (userGuess = prompt(`${userGuess} is not a number, enter a NUMBER.`));
 
-  if (validNumber > SECRET_NUMBER) {
-    userGuess = prompt(`Sorry ${userName}, Guess Lower.`);
-  }
+    if (validNumber > SECRET_NUMBER) {
+      userGuess = prompt(`Sorry ${userName}, Guess Lower.`);
+    } else if (validNumber < SECRET_NUMBER) {
+      userGuess = prompt(`Sorry ${userName}, Guess Higher.`);
+    } else {
+      if (userData.hasOwnProperty(userName)) {
+        if (userData[userName] > guessArr.length) {
+          alert(`Correct!\n ${userName}, you beat your previous score!`);
+        }
+      } else {
+        alert(`Correct! \nYour previous guesses were ${guessArr}!`);
+        userData[userName] = guessArr.length;
+      }
 
-  if (validNumber < SECRET_NUMBER) {
-    userGuess = prompt(`Sorry ${userName}, Guess Higher.`);
-  }
-
-  if (validNumber === SECRET_NUMBER) {
-    alert(`Correct! \nYour previous guesses were ${guessArr}!`);
-    playAgain = confirm(`Click "OK" to play again!`);
+      playAgain = confirm(`Click "OK" to play again!`);
+      if (playAgain) {
+        return playGame();
+      }
+    }
   }
 }
 
-if (playAgain){
-  location.reload()
-}
+playGame();
 
 function randomNumber(min, max) {
   return Math.floor(Math.random() * max - min + 1) + min;
